a83def24f820a0e2f7fb9e669d43bcf687dceb4f
Cody <cody.hennighan@gmail.com>
2019-05-14 15:12:47 +0800

    updated environment config

--- config/puma.rb
+++ config/puma.rb
@@ -13,7 +13,8 @@ port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+rails_env = ENV['RAILS_ENV'] || "production"
+environment rails_env
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together

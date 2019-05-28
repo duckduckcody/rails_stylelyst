c21d588d8d6c063d6a2197990f45a78f1ce6d0eb
Cody <cody.hennighan@gmail.com>
2019-05-28 09:58:25 +0800

    Made clothe controller cleaner

--- app/controllers/clothes_controller.rb
+++ app/controllers/clothes_controller.rb
@@ -2,12 +2,11 @@ class ClothesController < ApplicationController
   def index
     params[:page] = (params[:page] ||= '1')
     @page = params[:page].to_i # this is used for page nav. TODO: move this into javascript
-    @clothes = []
     @url_function = WebsiteUrlFunction.find_by(name: 'page')
     @user_settings = UserSettingsService.call(cookies)
-    @user_settings[:website_ids].each do |website_id|
-      @website = Website.find(website_id)
-      @clothes = @clothes.concat(@website.scrape(@user_settings, params, @url_function))
+    @clothes = @user_settings[:website_ids].each_with_object([]) do |website_id, clothes|
+      website = Website.find(website_id)
+      clothes.concat(website.scrape(@user_settings, params, @url_function))
     end
   end
 

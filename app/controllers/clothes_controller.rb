22f96805b6c2e993aaf94413f6458cb2fc73d4e7
Cody <cody.hennighan@healthengine.com.au>
2019-07-30 21:23:53 +0800

    Changed key and added get clothes interactor

--- app/controllers/clothes_controller.rb
+++ app/controllers/clothes_controller.rb
@@ -3,19 +3,8 @@ class ClothesController < ApplicationController
   end
 
   def get_clothes
-    clothes = []
-    if params[:page].present? & params[:category].present? & params[:websites].present?
-      page = params[:page]
-      category = params[:category]
-      websites = JSON.parse(params[:websites])
-      url_function = WebsiteUrlFunction.find_by(name: 'page')
-      clothes = websites.each_with_object([]) do |website_id, clothes|
-        website = Website.find(website_id)
-        clothes.concat(website.scrape(category, page, url_function))
-      end      
-    end
-    
-    render :json => {'clothes': clothes}
+    result = GetClothes.call(params)
+    render :json => {'clothes': result.clothes}
   end
 
   def get_settings

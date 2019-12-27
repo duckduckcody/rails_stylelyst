d6bb451cb220b6e368362cedfd065a0913d90146
Cody <cody.hennighan@healthengine.com.au>
2019-12-27 21:51:02 +0800

    removed bad seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,9 @@
-# url functions
-# 'male_categories', 'the_iconic', 'culture_kings', 'universal'
-fileNames = %w[
-  male_categories
-  female_categories
-  the_iconic
-  culture_kings
-  universal
-  beginning_botique
-]
+# the_iconic
+# culture_kings
+# universal
+# male_categories
+
+fileNames = %w[female_categories beginning_botique]
 fileNames.each do |fileName|
   require File.expand_path(File.dirname(__FILE__)) + "/seeds/#{fileName}.rb"
 end

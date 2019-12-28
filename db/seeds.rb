615a6d942b4ecaa396e7450733777647639069ff
Cody <cody.hennighan@healthengine.com.au>
2019-12-28 12:17:38 +0800

    Add the iconic to the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@ fileNames = %w[
   male_categories
   beginning_botique
   culture_kings
+  the_iconic
 ]
 fileNames.each do |fileName|
   require File.expand_path(File.dirname(__FILE__)) + "/seeds/#{fileName}.rb"
--- db/seeds/the_iconic.rb
+++ db/seeds/the_iconic.rb
@@ -1,17 +1,15 @@
-iconic_html_scraper = ScraperHtml.new(container_selector: 'div.product.columns')
-iconic_html_scraper.scraper_html_component_texts.new(
-  name: 'name', selector: 'span.name'
-)
-iconic_html_scraper.scraper_html_component_prices.new(
+iconic_scraper = Scraper.new(container_selector: 'div.product.columns')
+iconic_scraper.scraper_field_texts.new(name: 'name', selector: 'span.name')
+iconic_scraper.scraper_field_prices.new(
   name: 'price', selector: 'span.new-price,span.price'
 )
-iconic_html_scraper.scraper_html_component_prices.new(
+iconic_scraper.scraper_field_prices.new(
   name: 'old-price', selector: 'span.old-price'
 )
-iconic_html_scraper.scraper_html_component_images.new(
+iconic_scraper.scraper_field_images.new(
   name: 'image', selector: 'span.image-frame'
 )
-iconic_html_scraper.scraper_html_component_links.new(
+iconic_scraper.scraper_field_links.new(
   name: 'link', selector: 'a.product-details'
 )
 
@@ -19,74 +17,43 @@ the_iconic_website =
   Website.create!(
     name: 'The Iconic',
     url: 'https://www.theiconic.com.au',
+    scraper: iconic_scraper,
     query_string_key_page: 'page',
-    query_string_key_search: 'q'
+    query_string_key_search: 'q',
+    emulate_browser_get: false
   )
 # male
-the_iconic_website.website_url_htmls.create!(
-  url_extension: '/mens-clothing-tops',
-  website_url_function: @url_function_page,
-  category: @male_tops,
-  scraper_html: iconic_html_scraper
+the_iconic_website.website_pages.create!(
+  url_extension: '/mens-clothing-tops', category: @male_tops
 )
-the_iconic_website.website_url_htmls.create!(
-  url_extension: '/mens-clothing-pants',
-  website_url_function: @url_function_page,
-  category: @male_bottoms,
-  scraper_html: iconic_html_scraper
+the_iconic_website.website_pages.create!(
+  url_extension: '/mens-clothing-pants', category: @male_bottoms
 )
-the_iconic_website.website_url_htmls.create!(
-  url_extension: '/mens-clothing-coats-jackets',
-  website_url_function: @url_function_page,
-  category: @male_jackets,
-  scraper_html: iconic_html_scraper
+the_iconic_website.website_pages.create!(
+  url_extension: '/mens-clothing-coats-jackets', category: @male_jackets
 )
-the_iconic_website.website_url_htmls.create!(
-  url_extension: '/mens-clothing-sweats-hoodies',
-  website_url_function: @url_function_page,
-  category: @male_hoodies,
-  scraper_html: iconic_html_scraper
+the_iconic_website.website_pages.create!(
+  url_extension: '/mens-clothing-sweats-hoodies', category: @male_hoodies
 )
-the_iconic_website.website_url_htmls.create!(
-  url_extension: '/mens-sale',
-  website_url_function: @url_function_page,
-  category: @male_clearance,
-  scraper_html: iconic_html_scraper
+the_iconic_website.website_pages.create!(
+  url_extension: '/mens-sale', category: @male_clearance
 )
 # female
-the_iconic_website.website_url_htmls.create!(
-  url_extension: '/womens-clothing-tops',
-  website_url_function: @url_function_page,
-  category: @female_tops,
-  scraper_html: iconic_html_scraper
+the_iconic_website.website_pages.create!(
+  url_extension: '/womens-clothing-tops', category: @female_tops
 )
-the_iconic_website.website_url_htmls.create!(
-  url_extension: '/womens-clothing-dresses',
-  website_url_function: @url_function_page,
-  category: @female_dresses,
-  scraper_html: iconic_html_scraper
+the_iconic_website.website_pages.create!(
+  url_extension: '/womens-clothing-dresses', category: @female_dresses
 )
-the_iconic_website.website_url_htmls.create!(
-  url_extension: '/womens-clothing-pants',
-  website_url_function: @url_function_page,
-  category: @female_bottoms,
-  scraper_html: iconic_html_scraper
+the_iconic_website.website_pages.create!(
+  url_extension: '/womens-clothing-pants', category: @female_bottoms
 )
-the_iconic_website.website_url_htmls.create!(
-  url_extension: '/womens-clothing-coats-jackets',
-  website_url_function: @url_function_page,
-  category: @female_jackets,
-  scraper_html: iconic_html_scraper
+the_iconic_website.website_pages.create!(
+  url_extension: '/womens-clothing-coats-jackets', category: @female_jackets
 )
-the_iconic_website.website_url_htmls.create!(
-  url_extension: '/womens-clothing-sweats-hoodies',
-  website_url_function: @url_function_page,
-  category: @female_hoodies,
-  scraper_html: iconic_html_scraper
+the_iconic_website.website_pages.create!(
+  url_extension: '/womens-clothing-sweats-hoodies', category: @female_hoodies
 )
-the_iconic_website.website_url_htmls.create!(
-  url_extension: '/womens-sale',
-  website_url_function: @url_function_page,
-  category: @female_clearance,
-  scraper_html: iconic_html_scraper
+the_iconic_website.website_pages.create!(
+  url_extension: '/womens-sale', category: @female_clearance
 )

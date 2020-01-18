ed08b08574e8da0761052e6f0e4f3979f1294cbf
DuckDuckCody <cody.hennighan@gmail.com>
2020-01-18 12:42:26 +0800

    handle no category or websites set

--- app/controllers/clothes_controller.rb
+++ app/controllers/clothes_controller.rb
@@ -2,10 +2,15 @@ class ClothesController < ApplicationController
   def index; end
 
   def get_all_clothes
+    if !params[:category].present? || !params[:websites].present?
+      return render json: { 'clothes': []}
+    end
+
     website_pages =
       WebsitePage.where(
         category: params[:category], website: JSON.parse(params[:websites])
       )
+
     clothes =
       website_pages.each_with_object([]) do |website_page, clothes|
         clothes.concat(website_page.scrape_page(params[:page]))
